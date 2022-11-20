b4773e4ae96de4980e99199aba09837e853717f5
Prananya M <110041110+Prananya123@users.noreply.github.com>
2022-11-20 19:55:06 +0530

    Add files via upload

---
+++ Day 1(Full Adder)/fulladder.v
@@ -0,0 +1,76 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    19:54:00 11/13/2022 
+// Design Name: 
+// Module Name:    fulladder 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module fulladder( A, B, Cin, S, Cout);
+input wire A, B, Cin;
+output reg S, Cout;
+always @(A or B or Cin)
+begin
+ if(A==0 && B==0 && Cin==0)
+  begin
+   S=0;
+   Cout=0;
+  end
+
+ else if(A==0 && B==0 && Cin==1)
+  begin
+   S=1;
+   Cout=0;
+  end
+
+ else if(A==0 && B==1 && Cin==0)
+  begin
+   S=1;
+   Cout=0;
+  end
+
+ else if(A==0 && B==1 && Cin==1)
+  begin
+   S=0;
+   Cout=1;
+  end
+
+ else if(A==1 && B==0 && Cin==0)
+  begin
+   S=1;
+   Cout=0;
+  end
+
+ else if(A==1 && B==0 && Cin==1)
+  begin
+   S=0;
+   Cout=1;
+  end
+
+ else if(A==1 && B==1 && Cin==0)
+  begin
+   S=0;
+   Cout=1;
+  end
+
+ else if(A==1 && B==1 && Cin==1)
+  begin
+   S=1;
+   Cout=1;
+  end
+
+end
+
+endmodule
---
+++ Day 1(Full Adder)/fulladdertb.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   19:55:28 11/13/2022
+// Design Name:   fulladder
+// Module Name:   D:/verilog/fulladder/fulladdertb.v
+// Project Name:  fulladder
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: fulladder
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module fulladdtb;
+reg A,B,Cin;
+wire S,Cout;
+fulladder uut (.A(A),.B(B),.Cin(Cin),.S(S),.Cout(Cout));
+initial begin
+$monitor($time  ,"  A=%b   B=%b   Cin=%b   Cout=%b  S=%b   ",A,B,Cin,Cout,S);
+end
+initial begin
+ A=1'b0;B=1'b0;Cin=1'b0;
+#100 A=1'b0;B=1'b0;Cin=1'b1;
+#100 A=1'b0;B=1'b1;Cin=1'b0;
+#100 A=1'b0;B=1'b1;Cin=1'b1;
+#100 A=1'b1;B=1'b0;Cin=1'b0;
+#100 A=1'b1;B=1'b0;Cin=1'b1;
+#100 A=1'b1;B=1'b1;Cin=1'b0;
+#100 A=1'b1;B=1'b1;Cin=1'b1;
+#1000$stop;
+end
+endmodule
