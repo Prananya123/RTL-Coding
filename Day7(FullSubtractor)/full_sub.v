85532eb9876ee74e13b70330c15573603e73c7e5
Prananya M <110041110+Prananya123@users.noreply.github.com>
2022-11-26 23:29:37 +0530

    Add files via upload

---
+++ Day7(FullSubtractor)/full_sub.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:50:43 11/26/2022 
+// Design Name: 
+// Module Name:    full_sub 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module full_sub(X,Y,Z,Diff,Borr);
+output Diff, Borr;
+ input X, Y, Z;
+assign Diff = X ^ Y ^ Z;
+assign Borr = ~X & (Y^Z) | Y & Z;
+endmodule
---
+++ Day7(FullSubtractor)/full_sub_tb.v
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   22:54:37 11/26/2022
+// Design Name:   full_sub
+// Module Name:   D:/verilog/full_sub/full_sub_tb.v
+// Project Name:  full_sub
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: full_sub
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module full_sub_tb;
+
+	// Inputs
+	reg X;
+	reg Y;
+	reg Z;
+
+	// Outputs
+	wire Diff;
+	wire Borr;
+
+	// Instantiate the Unit Under Test (UUT)
+	full_sub uut (.X(X), .Y(Y), .Z(Z), .Diff(Diff), .Borr(Borr));
+initial begin
+    $monitor (  $time , "  X = %d    Y = %d   Z = %d    Borr = %d     Diff = %d",X, Y, Z, Borr, Diff);
+ 
+end
+	initial begin
+      X = 0; Y = 0; Z = 0;
+#100  X = 0; Y = 0; Z = 1;
+#100  X = 0; Y = 1; Z = 0;
+#100  X = 0; Y = 1; Z = 1;
+#100  X = 1; Y = 0; Z = 0;
+#100  X = 1; Y = 0; Z = 1;
+#100  X = 1; Y = 1; Z = 0;
+#100  X = 1; Y = 1; Z = 1;
+end
+endmodule
+
