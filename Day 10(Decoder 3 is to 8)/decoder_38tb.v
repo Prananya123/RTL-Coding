2c3c6b55de052d84fa9204bb70320a3f89c79789
Prananya M <110041110+Prananya123@users.noreply.github.com>
2022-11-29 21:01:22 +0530

    Add files via upload

---
+++ Day 10(Decoder 3 is to 8)/decoder_38tb.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   19:46:01 11/29/2022
+// Design Name:   decoder_38
+// Module Name:   D:/verilog/decoder_38/decoder_38tb.v
+// Project Name:  decoder_38
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: decoder_38
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+module decoder_tb;
+wire [7:0] out;
+reg en;
+reg [2:0] in;
+integer i;
+
+decoder_38 dut(in,out,en);
+
+initial begin  
+ $monitor( "en=%b, in=%d, out=%b ", en, in, out);
+   for ( i=0; i<16; i=i+1) 
+        begin
+           {en,in}  = i;
+            #1;
+        end
+end
+endmodule
