e392bfc705bb4d76915da46f9b99d06714d26c64
Prananya M <110041110+Prananya123@users.noreply.github.com>
2022-11-29 22:23:39 +0530

    Update fulladder.v

--- Day 1(Full Adder)/fulladder.v
+++ Day 1(Full Adder)/fulladder.v
@@ -1,21 +1,15 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer:Prananya M 
 // 
 // Create Date:    19:54:00 11/13/2022 
-// Design Name: 
-// Module Name:    fulladder 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
+// Design Name: Full Adder
+
+// Tool versions:ISE Design Suite 14.7 
+ 
 // Revision 0.01 - File Created
-// Additional Comments: 
+
 //
 //////////////////////////////////////////////////////////////////////////////////
 module fulladder( A, B, Cin, S, Cout);
