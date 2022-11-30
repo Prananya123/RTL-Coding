3c62cedc8ca26a336864a34067da78b0ec8c7224
Prananya M <110041110+Prananya123@users.noreply.github.com>
2022-11-30 20:55:09 +0530

    Add files via upload

---
+++ Day 11(Encoder 83)/encoder_83.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Author: Prananya M
+// Create Date:    18:47:19 11/30/2022 
+// Design Name: 8:3 ENCODER
+// Module Name:    encoder_83 
+// Tool versions: ISE Design Suite 14.7
+// Revision 0.01 - File Created
+//////////////////////////////////////////////////////////////////////////////////
+module encoder_83(in,out,en);
+input [7:0]in;
+input en;
+output [2:0]out;
+reg [2:0]out;
+always @(in or en)
+begin
+if (en==1) 
+		 begin
+          out=3'b000;
+  case (in) 
+ 8'd1:out=3'b000;
+ 8'd2:out=3'b001;
+ 8'd4:out=3'b010;
+ 8'd8:out=3'b011;
+ 8'd16:out=3'b100;
+ 8'd32:out=3'b101;
+ 8'd64:out=3'b110;
+ 8'd128:out=3'b111;
+ default:out=3'b000;
+          endcase
+      end
+else 
+out=3'b000;
+end 
+endmodule
---
+++ Day 11(Encoder 83)/encoder_83tb.v
@@ -0,0 +1,59 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   19:04:08 11/30/2022
+// Design Name:   encoder_83
+// Module Name:   D:/verilog/encoder_83/encoder_83tb.v
+// Project Name:  encoder_83
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: encoder_83
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module encoder_83tb;
+
+	// Inputs
+	reg [7:0] in;
+	reg en;
+	wire [2:0] out;
+	integer i;
+
+
+	// Instantiate the Unit Under Test (UUT)$$
+	encoder_83 uut (.in(in),.out(out),  .en(en));
+
+	
+  
+ initial begin  
+ $monitor($time, "    en=%b,    in=%d,    out=%b ", en, in, out);
+ end
+ initial begin
+   en=0;
+	i=0;
+	for(i=0;i<8;i=i+1)
+	begin
+	in=2**i;
+	#10;
+	end
+	#10;
+	en=1;
+	for(i=0;i<8;i=i+1)
+	begin
+	in=2**i;
+	#10;
+	end 
+	end
+	endmodule
+	
