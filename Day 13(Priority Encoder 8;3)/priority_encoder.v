b4c6af633c6ff2df904201430eed606def0f9016
Prananya M <110041110+Prananya123@users.noreply.github.com>
2022-12-02 21:58:39 +0530

    Add files via upload

---
+++ Day 13(Priority Encoder 8;3)/priority_encoder.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+//Author: Prananya M
+// Create Date:   20:47:54 12/02/2022
+// Design Name:   preority_encoder
+// Module Name:   D:/verilog/preority_encoder/preority_encodertb.v
+// Project Name:  preority_encoder  
+// Tool versions: ISE Drsign Suite 14.7 
+// Revision 0.01 - File Created
+//////////////////////////////////////////////////////////////////////////////////
+module preority_encoder(en,in,op);
+input en;
+input [7:0] in;
+output [2:0]op;
+reg [2:0]op;
+always @ (in,en)
+begin
+if(en==1)
+begin
+op=3'b000;
+casex (in)
+8'b00000001: op = 3'b000;
+8'b0000001x:op= 3'b001;
+8'b000001xx: op= 3'b010;
+8'b00001xxx: op= 3'b011;
+8'b0001xxxx: op= 3'b100;
+8'b001xxxxx: op= 3'b101;
+8'b01xxxxxx:op= 3'b110;
+8'b1xxxxxxx:op= 3'b111;
+default:op=3'b000;
+endcase
+end
+else
+op=3'b000;
+end
+endmodule
+
---
+++ Day 13(Priority Encoder 8;3)/priority_encodertb.v
@@ -0,0 +1,57 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+//Author: Prananya M
+// Create Date:   20:47:54 12/02/2022
+// Design Name:   preority_encoder
+// Module Name:   D:/verilog/preority_encoder/preority_encodertb.v
+// Project Name:  preority_encoder  
+// Tool versions: ISE Drsign Suite 14.7
+// Verilog Test Fixture created by ISE for module: preority_encoder
+// Revision 0.01 - File Created 
+////////////////////////////////////////////////////////////////////////////////
+
+module preority_encodertb;
+
+	// Inputs
+	reg en;
+	reg [7:0] in;
+
+	// Outputs
+	wire [2:0] op;
+
+	// Instantiate the Unit Under Test (UUT)
+	preority_encoder uut (
+		.en(en), 
+		.in(in), 
+		.op(op)
+	);
+
+	initial begin
+	$monitor ($time, "    in=%b   en=%b    op=%b",in,en,op);
+	end
+		initial begin
+		// Initialize Inputs
+		en = 0;
+ in=8'b00000001;
+#10; in=8'b0000001x;
+#10; in=8'b000001xx;
+#10; in=8'b00001xxx;
+#10; in=8'b0001xxxx;
+#10; in=8'b001xxxxx;
+#10; in=8'b01xxxxxx;
+#10; in=8'b1xxxxxxx;
+#10; en = 1;
+ in=8'b00000001;
+#10; in=8'b0000001x;
+#10; in=8'b000001xx;
+#10; in=8'b00001xxx;
+#10; in=8'b0001xxxx;
+#10; in=8'b001xxxxx;
+#10; in=8'b01xxxxxx;
+#10; in=8'b1xxxxxxx;
+#10 $finish;		
+	end
+      
+endmodule
+
