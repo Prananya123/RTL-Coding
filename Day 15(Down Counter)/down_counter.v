45ff697946a4792f92a397f37b037405ec83c48b
Prananya M <110041110+Prananya123@users.noreply.github.com>
2022-12-04 22:15:39 +0530

    Add files via upload

---
+++ Day 15(Down Counter)/down_counter.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:19:21 12/04/2022 
+// Design Name: 
+// Module Name:    down_counter 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module down_counter(clk,rst,count);
+input rst,clk;
+output [3:0]count;
+reg [3:0]count;
+ always @ (posedge clk or negedge rst)
+ begin  
+    if ( rst==0)  
+      count <= 4'b1111;  
+    else  
+      count <= count - 1;  
+  end  
+endmodule
+
+
---
+++ Day 15(Down Counter)/down_counter_tb.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   21:23:40 12/04/2022
+// Design Name:   down_counter
+// Module Name:   D:/verilog/down_counter/down_counter_tb.v
+// Project Name:  down_counter
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: down_counter
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module down_counter_tb;
+	reg clk;
+	reg rst;
+	wire [3:0] count;
+	down_counter uut (
+		.clk(clk), 
+		.rst(rst), 
+		.count(count)
+	);
+
+	initial begin
+	$monitor ($time, "  count=%b",count);
+	end 
+		initial begin
+		// Initialize Inputs
+		clk = 0;
+		rst = 0; 
+
+		// Wait 100 ns for global reset to finish
+		#20;
+		rst = 1;
+end
+ always 
+ #5 clk=~clk;  
+endmodule
+
+
