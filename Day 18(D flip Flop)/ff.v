0656a14f55b796e44f74a3547c55826073d81bec
Prananya M <110041110+Prananya123@users.noreply.github.com>
2022-12-07 23:38:38 +0530

    Add files via upload

---
+++ Day 18(D flip Flop)/ff.v
@@ -0,0 +1,59 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:06:46 12/07/2022 
+// Design Name: 
+// Module Name:    ff 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module DFF( Q,
+
+  Qbar,
+
+  D,
+
+  Clk,
+
+  Reset
+
+    );
+
+output  reg  Q; 
+
+output   Qbar; 
+
+input   D, 
+
+  Clk, 
+
+  Reset; 
+
+assign Qbar = ~Q; 
+
+always @(posedge Clk) 
+
+begin 
+
+ if (Reset == 1'b1) //If not at reset 
+
+  Q = 1'b0;
+
+ else 
+
+  Q = D;
+
+end 
+
+endmodule
---
+++ Day 18(D flip Flop)/fftb.v
@@ -0,0 +1,84 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   23:14:49 12/07/2022
+// Design Name:   DFF
+// Module Name:   D:/verilog/ff/fftb.v
+// Project Name:  ff
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: DFF
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+module DFF_tb;
+
+ // Inputs
+
+ reg D;
+
+ reg Clk;
+
+ reg Reset;
+
+ // Outputs
+
+ wire Q;
+
+ wire Qbar;
+
+ // Instantiate the Unit Under Test (UUT)
+
+ DFF uut (
+
+  .Q(Q), 
+
+  .Qbar(Qbar), 
+
+  .D(D), 
+
+  .Clk(Clk), 
+
+  .Reset(Reset)
+
+  );
+
+ initial begin
+ $monitor( " D=%b  Q=%b  Qbar=%b",D,Q,Qbar);
+ end
+ initial begin
+  // Initialize Inputs
+
+  D  = 1'b0;
+
+  Clk  = 1'b0;
+
+  Reset   = 1'b1;
+
+  // Wait 100 ns for global reset to finish
+
+  #100;
+
+  // Add stimulus here
+
+  Reset = 1'b0;
+
+  #20;
+
+  forever #40 D = ~ D;
+
+ end
+
+   always #10 Clk = ~Clk;  
+
+endmodule
