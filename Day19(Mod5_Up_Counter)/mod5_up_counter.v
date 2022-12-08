a9558a6128b922a7f3ec9124fc3e0854e273f0b1
Prananya M <110041110+Prananya123@users.noreply.github.com>
2022-12-09 00:25:12 +0530

    Add files via upload

---
+++ Day19(Mod5_Up_Counter)/mod5_up_counter.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:45:07 12/08/2022 
+// Design Name: 
+// Module Name:    mod5_up_counter 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module mod5_vlog(
+ out      ,  // Output of the counter
+ clk      ,  // clock input
+ reset       // reset input
+ );
+ //----------Output Ports--------------
+  output [2:0] out;
+  //------------Input Ports--------------
+  input clk, reset;
+  //------------Internal Variables--------
+  reg [2:0] out;
+  //-------------Code Starts Here-------
+  always @(posedge clk)
+ if (reset)
+  begin // active high reset
+   out <= 2'b0 ;
+  end
+ else if (out<4)
+  begin
+   out <= out + 1;
+  end
+ else
+  begin
+   out <= 2'b0 ;
+  end
+
+  endmodule
+  
---
+++ Day19(Mod5_Up_Counter)/mod5_up_countertb.v
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   23:45:58 12/08/2022
+// Design Name:   mod5_vlog
+// Module Name:   D:/verilog/mod5_up_counter/mod5_up_countertb.v
+// Project Name:  mod5_up_counter
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: mod5_vlog
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module tf_mod5_vlog;
+
+ // Inputs
+ reg clk;
+ reg reset;
+
+ // Outputs
+ wire [2:0] out;
+
+ // Instantiate the Unit Under Test (UUT)
+ mod5_vlog uut (
+  .out(out),
+  .clk(clk),
+  .reset(reset)
+ );
+
+ initial begin
+  clk=1'b1;
+  forever #5 clk=~clk;
+ end
+
+
+ initial begin
+  reset = 1;#10;
+  reset = 0;#30;
+  
+ end
+   
+endmodule
