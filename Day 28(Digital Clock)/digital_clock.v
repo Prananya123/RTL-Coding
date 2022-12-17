85b96574b6c608ac0edce08f824b5c19dc3ca74b
Prananya M <110041110+Prananya123@users.noreply.github.com>
2022-12-17 21:51:16 +0530

    Add files via upload

---
+++ Day 28(Digital Clock)/digital_clock.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//Author:Prananya M
+// Create Date:   20:01:29 12/17/2022
+// Design Name:   digital clock
+// Module Name:   D:/verilog/dig_clk/dig_clk_tb.v
+// Project Name:  dig_clk
+////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+module  dig_clk(clk,rst,sec,min,hr);
+input clk,rst;
+output [6:0]sec,min;
+output [4:0]hr;
+reg [6:0]sec,min;
+reg [4:0]hr;
+always @(posedge clk)
+begin
+if(rst==1)
+begin
+	sec=0;
+	min=0;
+	hr=0;
+end
+else
+begin
+	sec=sec+1;
+		if(sec==60)
+		begin
+		min=min+1;
+		sec=0;
+		end
+			if(min==60)
+			begin
+			hr=hr+1;
+			min=0;
+			end
+				if(hr==24)
+				begin
+				hr=0;
+				min=0;
+				sec=0;
+				end
+
+end	
+end
+endmodule
---
+++ Day 28(Digital Clock)/digital_clock_tb.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+//Author:Prananya M
+// Create Date:   20:01:29 12/17/2022
+// Design Name:   digital clock
+// Module Name:   D:/verilog/dig_clk/dig_clk_tb.v
+// Project Name:  dig_clk
+////////////////////////////////////////////////////////////////////////////////
+
+
+module dig_clk_tb;
+
+	// Inputs
+	reg clk;
+	reg rst;
+
+	// Outputs
+	wire [6:0] sec;
+	wire [6:0] min;
+	wire [4:0] hr;
+
+	// Instantiate the Unit Under Test (UUT)
+	dig_clk uut (
+		.clk(clk), 
+		.rst(rst), 
+		.sec(sec), 
+		.min(min), 
+		.hr(hr)
+	);
+initial
+begin
+$monitor($time,"  sec=%d    min=%d   hour=%d  ",sec,min,hr);
+end
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		rst = 1;
+		#100 rst=0;
+        
+		// Add stimulus here
+
+	end
+      always
+		#1clk=~clk;
+endmodule
+
+
